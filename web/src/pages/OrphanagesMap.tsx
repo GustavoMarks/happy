dd24bfa5b41fb8bf55b08df19ad6480b23f95e08
Gustavo Marques <gustavosmarquesf@gmail.com>
2020-10-14 00:16:47 -0300

    (workshop 1) tela de mapeamento de orfanatos

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function OrphanagesMap(){
-  return(
-    <h1> Hello World </h1>
+import { Map, TileLayer } from 'react-leaflet';
+
+import mapMarker from '../images/Map-marker.svg';
+import { FiPlus } from "react-icons/fi";
+
+import 'leaflet/dist/leaflet.css';
+import '../styles/pages/orphanages.css';
+
+export default function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarker} alt='Happy' />
+
+          <h2> Escolha um orfanato no mapa </h2>
+          <p> Muitas crianças estão esperando a sua visita :) </p>
+        </header>
+
+        <footer>
+          <strong> Fortaleza </strong>
+          <span> Ceará </span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-3.7766171, -38.4954941]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+      <Link to="" className="create-orphanage" >
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+
+    </div>
   )
 }
