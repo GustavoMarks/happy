8fe22df0d82496aec6f2d3737e4032091aa9dcd5
Gustavo Marques <gustavosmarquesf@gmail.com>
2020-10-24 17:01:17 -0300

    (workshop 4) organizando rotas da versão mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker, Callout } from "react-native-maps";
-
-import { Feather } from "@expo/vector-icons";
-import mapMarker from './src/Images/mapMarker.png';
 
 import { useFonts } from "expo-font";
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from "@expo-google-fonts/nunito";
+
+import Routes from './src/routes';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -16,103 +13,7 @@ export default function App() {
   if(!fontsLoaded) return null;
 
   return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: -27.2092052,
-          longitude: -49.6401092,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-      >
-        <Marker
-          icon={mapMarker}
-          coordinate={{
-            latitude: -27.2092052,
-            longitude: -49.6401092,
-          }}
-          calloutAnchor={{
-            x: 2.7,
-            y: 0.8,
-          }}
-        >
-          <Callout tooltip onPress={() => alert('oi')}>
-            <View style={styles.calloutCotainer} >
-              <Text style={styles.calloutText} > Lar das Meninas </Text>
-            </View>
-
-          </Callout>
-        </Marker>
-      </MapView>
-
-      <View style={styles.footer} >
-        <Text style={styles.footerText}> 2 Orfanatos encontrados </Text>
-
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }} >
-          <Feather name="plus" size={20} color="#FFF" />
-        </TouchableOpacity>
-      </View>
-    </View>
+   <Routes/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-  },
-
-  calloutCotainer: {
-    width: 160,
-    height: 46,
-    paddingHorizontal: 16,
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    borderRadius: 16,
-    justifyContent: 'center',
-  },
-
-  calloutText: {
-    fontFamily: 'Nunito_700Bold',
-    color: '#0089a5',
-    fontSize: 14,
-  },
-
-  footer: {
-    position: 'absolute',
-    left: 24,
-    right: 24,
-    bottom: 52,
-
-    backgroundColor: '#FFF',
-    borderRadius: 20,
-    height: 56,
-    paddingLeft: 24,
-
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: "center",
-
-    elevation: 3,
-  },
-
-  footerText: {
-    fontFamily: 'Nunito_700Bold',
-    color: "#8fe7b5",  
-  },
-
-  createOrphanageButton: {
-    width: 56,
-    height: 56,
-    backgroundColor: '#15c3d6',
-    borderRadius: 20,
-
-    justifyContent: 'center',
-    alignItems: "center"
-  }
-});
