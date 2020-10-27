e60a262232ed2ce349d0e9b094dea9e58c20e7c1
Gustavo Marques <gustavosmarquesf@gmail.com>
2020-10-27 20:43:29 -0300

    configurando scripts para deploy do backend

---
+++ backend/copyFilesScript.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+console.log("[*] Copiando banco de dados sqlite para build...");
+fs.copyFile('./src/database/database.sqlite', './src/database/database.sqlite', (err) => {
+  if (err){
+    console.log('[!] Erro ao tentar restaurar banco de dados.');
+    throw err;
+  }
+  console.log('[*] Banco de dados copiado com sucesso');
+});
